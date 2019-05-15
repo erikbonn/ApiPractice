249cf7e92f4f9f33cb10ad314430ea06fe83e5e9
Erik Bonn <erik.bonn@biofiredx.com>
2019-05-15 16:53:31 -0600

    add poke api and pug

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 const express = require('express')
 const app = express()
+// const pug = require('pug')
+let fetch = require('node-fetch')
+app.set('view engine', 'pug')
+app.set("views", __dirname)
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const getPokemon = () => {
+  return new Promise(function (resolve, reject) {
+    fetch('https://pokeapi.co/api/v2/pokemon/', {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    }).then(response => {
+      return response.json()
+    }).then(data => {
+      console.log(data)
+      resolve(data)
+      // res.send(data)
+    }).catch(err => {
+      console.log(err)
+      reject(err)
+    });
+  })
+}
+
+app.get('/', (req, res, next) => {
+  getPokemon().then(data => {
+    res.render("index", {
+      namesAndUrls: data.results.map((poke, index) => {
+        return [index, poke.name, poke.url]
+      })
+    })
+  })
+  // res.sendFile(__dirname + '/index.pug', {name: 'datum'})
 })
 
 app.listen(8080, () => {
   console.log('Example app listening on port 8080!')
 })
+
+// ul
+//         for name in names 
+//           li= name 
